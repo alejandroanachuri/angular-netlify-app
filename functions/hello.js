9295a9cc01e613f91d6e65a254d21a3c4264b13a
Alejandro Anachuri <alejandroanachuri@gmail.com>
2022-05-04 23:32:23 -0300

    Query database

--- functions/hello.js
+++ functions/hello.js
@@ -11,12 +11,10 @@ const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 exports.handler = async function(event, context) {
   const { data, error } = await supabase
   .from('todos')
-  .insert([
-      { description: 'Todo genesis' },
-  ]);
+  .select('*');
   console.log(data, error);
   return {
     statusCode:200,
-    body: JSON.stringify({message:"Welcome to cloud functions","dburl":DATABASE_URL,"apikey":SUPABASE_SERVICE_API_KEY})
+    body: JSON.stringify({message:"Welcome to cloud functions",data:data})
   }
 }
