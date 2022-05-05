8f8d3a2828d17c9b6e5f1a5283932a16cbbc2a7c
Alejandro Anachuri <alejandroanachuri@gmail.com>
2022-05-04 23:19:02 -0300

    supabase integration

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+const {
+    DATABASE_URL,
+    SUPABASE_SERVICE_API_KEY
+} = process.env;
+
+// Connect to our database 
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
 exports.handler = async function(event, context) {
+  const { data, error } = await supabase
+  .from('todos')
+  .insert([
+      { description: 'Todo genesis' },
+  ]);
+  console.log(data, error);
   return {
     statusCode:200,
-    body: JSON.stringify({message:"Welcome to cloud functions"})
+    body: JSON.stringify({message:"Welcome to cloud functions","dburl":DATABASE_URL,"apikey":SUPABASE_SERVICE_API_KEY})
   }
 }
